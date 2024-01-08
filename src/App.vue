d4af0fd638e3b8618bf1a4e23650e1c9b20fb93c
philemon <philemonabakahmensah@gmail.com>
2024-01-08 00:18:24 +0000

    feat: done with understanding props in vuew

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>{{ title }}</h2>
+  <Modal :header="header" :description="description" theme="sale" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: 'My First vuejs App',
+      header: 'Signup Modal',
+      description: 'This is signup modal, please work with us'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
 }
 </script>
@@ -22,5 +36,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h2{
+  border: 1px solid black;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="backdrop">
+        <div class="modal" :class="{sale : theme === 'sale'}">
+            <h1>{{  header }}</h1>
+            <p>{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['header', 'description', 'theme'],
+}
+</script>
+
+<style>
+.modal{
+    width: 400px;
+    padding: 20px;
+    margin: 10px auto;
+    background: #fff;
+    border-radius: 10px;
+}
+
+.modal.sale{
+    background: crimson;
+    color: #fff;
+}
+
+.modal.sale h1{
+    color: #fff;
+}
+
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: #222;
+    width: 100%;
+    height: 100%;
+}
+
+.modal p{
+    font-size: large;
+}
+
+.modal h1{
+    color: rgb(67, 183, 183);
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 6px;
+    font: 2em sans-serif;
+    font-weight: 700;
+}
+</style>
